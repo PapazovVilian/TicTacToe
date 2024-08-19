44ca9cfb73bcc9678cb5bca9c4d20413e45ca2a5
PapazovVilian <papazov.vilian@gmx.de>
2024-08-19 20:45:46 +0200

    anti-diagonal win

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -133,6 +133,18 @@ public class TicTacToe {
             gameOver = true;
             return;
         }
+        //anti-diagonally
+
+        if(board[0][2].getText() == board[1][1].getText() &&
+            board[1][1].getText() == board[2][0].getText() &&
+            board[0][2].getText() != "") {
+            setWinner(board[0][2]);
+            setWinner(board[1][1]);
+            setWinner(board[2][0]);
+            
+            gameOver = true;
+            return;
+        }
         
         
     }
